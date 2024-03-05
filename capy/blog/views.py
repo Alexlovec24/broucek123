aa8dc566612c9cc69bd3ca35f34969ecc0b2d77f
Alex <alexnolfseznam.cz>
2024-03-05 08:38:56 +0100

    commit generator

--- capy/blog/views.py
+++ capy/blog/views.py
@@ -4,5 +4,5 @@ import random
 from lorem_text import lorem
 
 def blog_list(request):  # Add the 'request' parameter here
-    random_text = lorem.words(10)
-    return render(request, 'blog/list.html', {'random_text': random_text})  # Use 'request' in render and fix context dictionary
+    random_number = random.randint(a = 1, b = 300)
+    return render(request, 'blog/list.html', {'random_number': random_number})  # Use 'request' in render and fix context dictionary
